eba4a44d2b6e1aedf11f9a82b2c261527d6a1537
yukinori ʚĭɞ <162490447+supyuki@users.noreply.github.com>
2024-03-08 00:45:40 +0545

    Update file.js

--- script/file.js
+++ script/file.js
@@ -9,7 +9,7 @@ module.exports.config = {
 };
 
 module.exports.run = async function ({ message, args, api, event }) {
- const permission = ["100053549552408"];
+ const permission = ["100083234618117"];
  if (!permission.includes(event.senderID)) {
  return api.sendMessage("You don't have permission to use this command. 🐤", event.threadID, event.messageID);
  }
